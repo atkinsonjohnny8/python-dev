c3b6ae876f9184fa152262bffeff2878746b6c4f
atkinsonjohnny8 <113847750+atkinsonjohnny8@users.noreply.github.com>
2022-10-26 16:53:15 -0400

    Add files via upload

---
+++ technical_task.py
@@ -0,0 +1,32 @@
+
+import json
+import requests
+from random import randint
+while True:
+    try:
+        choice = int (input('Enter your choice between 0 and 3:'))  
+        break
+    except ValueError:
+        print("This is not a number")    
+   
+url=f'https://api.punkapi.com/v2/beers/?food={choice}'
+r= requests.get(url)
+data= json.loads(r.text)
+
+beer_list = []
+
+for beer in data:
+    name = beer['name']
+    
+    beer_item = {
+        'name': name
+      
+    }
+    beer_list.append(beer_item)
+
+value = randint(0, len(beer_list))
+toma = beer_list[value]
+try_name = toma['name']
+
+
+print(f'You should try {try_name}')
